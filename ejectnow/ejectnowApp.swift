7ee8db86cde44b5fc4d424b260884af668ba62a7
Andrew Bacon <abaconfl@gmail.com>
2026-07-15 20:42:53 -0400

    Initial Commit

---
+++ ejectnow/ejectnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejectnowApp.swift
+//  ejectnow
+//
+//  Created by Andrew Bacon on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ejectnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
